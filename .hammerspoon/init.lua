0818ab6a270bae9dad95dc42eec3f3e5e68bd238
quangd42 <quangdn42@gmail.com>
2025-06-17 21:23:45 -0400

    misc

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,5 +1,5 @@
 hs.loadSpoon("SpoonInstall")
 
-hs.loadSpoon("ControlEscape"):start() -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
+hs.loadSpoon("ControlEscape"):start(200) -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
 
 hs.loadSpoon("FnViNav"):start()
