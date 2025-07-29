4accb913d2954975886df0e9a9faae3e318e04d0
quangdn42 <quangdn42@gmail.com>
2025-07-29 13:41:34 -0400

    update dial.nvim

--- .config/nvim/lua/plugins/dial.lua
+++ .config/nvim/lua/plugins/dial.lua
@@ -7,8 +7,7 @@ function M.dial(increment, g)
   -- Use visual commands for VISUAL 'v', VISUAL LINE 'V' and VISUAL BLOCK '\22'
   local is_visual = mode == 'v' or mode == 'V' or mode == '\22'
   local func = (increment and 'inc' or 'dec') .. (g and '_g' or '_') .. (is_visual and 'visual' or 'normal')
-  local group = vim.g.dials_by_ft[vim.bo.filetype] or 'default'
-  return require('dial.map')[func](group)
+  return require('dial.map')[func]()
 end
 
 return {
@@ -22,7 +21,7 @@ return {
     { "g<C-a>", function() return M.dial(true, true) end, expr = true, desc = "Increment", mode = {"n", "v"} },
     { "g<C-x>", function() return M.dial(false, true) end, expr = true, desc = "Decrement", mode = {"n", "v"} },
   },
-  opts = function()
+  config = function()
     local augend = require 'dial.augend'
 
     local logical_symbols = augend.constant.new {
@@ -40,18 +39,7 @@ return {
     local ordinal_numbers = augend.constant.new {
       -- elements through which we cycle. When we increment, we go down
       -- On decrement we go up
-      elements = {
-        'first',
-        'second',
-        'third',
-        'fourth',
-        'fifth',
-        'sixth',
-        'seventh',
-        'eighth',
-        'ninth',
-        'tenth',
-      },
+      elements = { 'first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth' },
       -- if true, it only matches strings with word boundary. firstDate wouldn't work for example
       word = false,
       -- do we cycle back and forth (tenth to first on increment, first to tenth on decrement).
@@ -60,34 +48,13 @@ return {
     }
 
     local weekdays = augend.constant.new {
-      elements = {
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday',
-      },
+      elements = { 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday' },
       word = true,
       cyclic = true,
     }
 
     local months = augend.constant.new {
-      elements = {
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December',
-      },
+      elements = { 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' },
       word = true,
       cyclic = true,
     }
@@ -101,78 +68,39 @@ return {
       cyclic = true,
     }
 
-    return {
-      dials_by_ft = {
-        css = 'css',
-        vue = 'vue',
-        javascript = 'typescript',
-        typescript = 'typescript',
-        typescriptreact = 'typescript',
-        javascriptreact = 'typescript',
-        json = 'json',
-        lua = 'lua',
-        markdown = 'markdown',
-        sass = 'css',
-        scss = 'css',
-        python = 'python',
-      },
-      groups = {
-        default = {
-          augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
-          augend.integer.alias.decimal_int, -- nonnegative and negative decimal number
-          augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
-          augend.date.alias['%Y/%m/%d'], -- date (2022/02/19, etc.)
-          ordinal_numbers,
-          weekdays,
-          months,
-          capitalized_boolean,
-          augend.constant.alias.bool, -- boolean value (true <-> false)
-          logical_symbols,
-        },
-        vue = {
-          augend.constant.new { elements = { 'let', 'const' } },
-          augend.hexcolor.new { case = 'lower' },
-          augend.hexcolor.new { case = 'upper' },
-        },
-        typescript = {
-          augend.constant.new { elements = { 'let', 'const' } },
-        },
-        css = {
-          augend.hexcolor.new {
-            case = 'lower',
-          },
-          augend.hexcolor.new {
-            case = 'upper',
-          },
-        },
-        markdown = {
-          augend.misc.alias.markdown_header,
-        },
-        json = {
-          augend.semver.alias.semver, -- versioning (v1.1.2)
-        },
-        lua = {
-          logical_words,
-        },
-        zig = {
-          logical_words,
-        },
-        python = {
-          augend.constant.new {
-            elements = { 'and', 'or' },
-          },
-        },
-      },
+    local var_const = augend.constant.new { elements = { 'var', 'const' } }
+
+    local default = {
+      augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
+      augend.integer.alias.decimal_int, -- nonnegative and negative decimal number
+      augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
+      augend.integer.alias.octal,
+      augend.integer.alias.binary,
+      augend.date.alias['%Y/%m/%d'], -- date (2022/02/19, etc.)
+      ordinal_numbers,
+      weekdays,
+      months,
+      augend.constant.alias.bool, -- boolean value (true <-> false)
+      logical_symbols,
+      logical_words,
     }
-  end,
-  config = function(_, opts)
-    -- copy defaults to each group
-    for name, group in pairs(opts.groups) do
-      if name ~= 'default' then
-        vim.list_extend(group, opts.groups.default)
-      end
+
+    ---@param augends Augend[]
+    ---@return Augend[]
+    local function add(augends)
+      return vim.list_extend(default, augends)
     end
-    require('dial.config').augends:register_group(opts.groups)
-    vim.g.dials_by_ft = opts.dials_by_ft
+
+    require('dial.config').augends:register_group {
+      default = default,
+    }
+    require('dial.config').augends:on_filetype {
+      markdown = add { augend.misc.alias.markdown_header },
+      json = add {
+        augend.semver.alias.semver, -- versioning (v1.1.2)
+      },
+      zig = add { var_const },
+      python = add { capitalized_boolean },
+    }
   end,
 }
