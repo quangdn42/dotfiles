14721434e92fb12f6c62c98071a8ad807ddbad54
quangdn42 <quangdn42@gmail.com>
2026-06-03 20:16:07 -0400

    use kanata instead of hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,7 +1,7 @@
 hs.loadSpoon("SpoonInstall")
 
-hs.loadSpoon("ControlEscape"):start(200) -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
+-- hs.loadSpoon("ControlEscape"):start(200) -- Load Hammerspoon bits from https://github.com/jasonrudolph/ControlEscape.spoon
 
-hs.loadSpoon("FnViNav"):start()
+-- hs.loadSpoon("FnViNav"):start()
 
 -- require("chromium_omnibox_hotkeys").setup()
