6a8f1374884d232492cfd8d8a438aaa53af45674
quangdn42 <quangdn42@gmail.com>
2026-07-13 12:58:33 -0400

    move some back to mac only

--- macos/.config/lazyvim/lua/plugins/colorscheme.lua
+++ macos/.config/lazyvim/lua/plugins/colorscheme.lua
@@ -3,6 +3,17 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
+    opts = {
+      style = "moon",
+      transparent = false,
+      on_highlights = function(hl, colors)
+        hl.MiniFilesTitleFocused = {
+          bg = colors.yellow,
+          fg = colors.black,
+        }
+        hl.InclineNormal = { fg = colors.yellow, bg = colors.black }
+      end,
+    },
   },
   -- Configure LazyVim to load colorscheme
   {
--- macos/.config/nvim/lua/plugins/colorscheme.lua
+++ macos/.config/nvim/lua/plugins/colorscheme.lua
@@ -2,10 +2,22 @@ return {
   {
     'folke/tokyonight.nvim',
     lazy = false,
+    version = '*',
     priority = 1000,
     init = function()
       vim.cmd.colorscheme 'tokyonight-night'
     end,
+    opts = {
+      transparent = false,
+      on_highlights = function(hl, colors)
+        hl['SnacksDashboardHeader'] = { fg = colors.blue2 }
+        hl['WinBar'] = { bg = colors.bg }
+        hl['WinBarNC'] = { bg = colors.bg }
+      end,
+      on_colors = function(colors)
+        colors.border = colors.blue2
+      end,
+    },
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
